c1390d214960d8f4b2aa7221fb2de72a224b0e6e
Nick Verbos <nickmastav@hotmail.com>
2015-04-29 11:59:00 -0700

    Added IsSentTo.py

---
+++ IsSentTo.py
@@ -0,0 +1,19 @@
+import hashlib
+
+class IsSentTo:
+   def __init__(self, fk_campaign_name, fk_email_id):
+      self.fk_campaign_name = fk_campaign_name
+      self.fk_email_id = fk_email_id
+
+   def __eq__(self, other):
+      if not isinstance(other, IsSentTo):
+         return False
+      
+      return (self.fk_campaign_name == other.fk_campaign_name and
+              self.fk_email_id == other.fk_email_id)
+
+   def __str__(self):
+      return   '"' + str(self.fk_campaign_name) + '", ' + str(self.fk_email_id)
+
+   def __hash__(self):
+      return hash(self.fk_campaign_name + fk_email_id)
